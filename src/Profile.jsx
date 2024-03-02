b5edb301ef1b70809ada08a4167253588131a23b
JonathanSanon <jonathancsanon@gmail.com>
2024-03-02 12:14:47 -0500

    Challenge 1 of 1: Split the components further solved

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,3 +1,3 @@
-export default function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+export function Profile() {
+  return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />;
 }
