78c360a574b4ab9d25e2e4139948c389d88a3a2b
JonathanSanon <jonathancsanon@gmail.com>
2024-03-07 22:34:46 -0500

    Challenge 3 of 4: Extracting a list item component

--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -1,45 +1,49 @@
 import { recipes } from "./data.js";
 
-export default function RecipeList() {
-  const salads = recipes.map((food, index) => (
-    <div key={index}>
-      <h2>{food.name}</h2>
-
-      <ul>
-        {food.ingredients.map((ingredient, n) => (
-          <li key={n}>{ingredient}</li>
-        ))}
-      </ul>
-    </div>
-  ));
-
+function Recipe({ id, name, ingredients }) {
   return (
     <div>
-      <h1>Recipes</h1>
-      <>{salads}</>
+      <h2>{name}</h2>
+
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-// solution
-// import { recipes } from './data.js';
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => (
+        <Recipe {...recipe} key={recipe.id} />
+        // {...recipe} <- spread operator
+      ))}
+    </div>
+  );
+}
 
+// solution i came up with challenge 2
 // export default function RecipeList() {
+//   const salads = recipes.map((food, index) => (
+//     <div key={index}>
+//       <h2>{food.name}</h2>
+
+//       <ul>
+//         {food.ingredients.map((ingredient, n) => (
+//           <li key={n}>{ingredient}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   ));
+
 //   return (
 //     <div>
 //       <h1>Recipes</h1>
-//       {recipes.map(recipe =>
-//         <div key={recipe.id}>
-//           <h2>{recipe.name}</h2>
-//           <ul>
-//             {recipe.ingredients.map(ingredient =>
-//               <li key={ingredient}>
-//                 {ingredient}
-//               </li>
-//             )}
-//           </ul>
-//         </div>
-//       )}
+//       <>{salads}</>
 //     </div>
 //   );
 // }
